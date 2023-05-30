39a983f5b376041e5bb03ce9ff4202b740bc5cf5
PMajerczyk <pawel.majerczyk@onet.eu>
2023-05-30 23:18:27 +0200

    Update

---
+++ Ludo/board&pawns.cpp
@@ -0,0 +1,29 @@
+#include "board&pawns.h"
+
+void Board::field(float x, float y, int c1, int c2, int c3)
+{
+    RectangleShape::setFillColor(sf::Color(c1, c2, c3));
+    RectangleShape::setSize(sf::Vector2f(100, 100));
+    RectangleShape::setOutlineThickness(5);
+    RectangleShape::setOutlineColor(sf::Color::Black);
+    RectangleShape::setPosition(x, y);
+}
+
+
+void Pawn::pawn(bool light)
+{
+    CircleShape::setRadius(40);
+    if (color == "Red") CircleShape::setFillColor(sf::Color::Red);
+    if (color == "Blue") CircleShape::setFillColor(sf::Color::Blue);
+    if (color == "Yellow") CircleShape::setFillColor(sf::Color::Yellow);
+    if (color == "Green") CircleShape::setFillColor(sf::Color::Green);
+    if (light == true) {
+        CircleShape::setOutlineThickness(7);
+        CircleShape::setOutlineColor(sf::Color(255, 255, 255));
+    }
+    else {
+        CircleShape::setOutlineThickness(5);
+        CircleShape::setOutlineColor(sf::Color::Black);
+    }
+    CircleShape::setPosition(X, Y);
+}
---
+++ Ludo/board&pawns.h
@@ -0,0 +1,18 @@
+#include <SFML/Graphics.hpp>
+#include <string.h>
+
+class Board : public sf::RectangleShape
+{
+	public:
+	void field(float , float, int=200, int=200, int=100);
+};
+
+class Pawn : public sf::CircleShape
+{
+	public:
+	int X;
+	int Y;
+	std::string color;
+	int index;
+	void pawn(bool=false);
+};
